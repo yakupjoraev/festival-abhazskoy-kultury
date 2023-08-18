600ef593bee3030cd65fd45b2cf5521d48f36452
Yakup <yakupjoraev@gmail.com>
2023-08-18 09:43:19 +0300

    added new sections

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,20 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".how__arrow--prev",
   },
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButtons = document.querySelectorAll(".toggle-button");
+
+  toggleButtons.forEach(button => {
+    button.addEventListener("click", function () {
+      const textBlock = this.previousElementSibling;
+      textBlock.classList.toggle("more");
+
+      if (textBlock.classList.contains("more")) {
+        this.textContent = "Свернуть";
+      } else {
+        this.textContent = "Подробнее";
+      }
+    });
+  });
+});
