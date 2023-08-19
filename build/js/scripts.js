258d4db3ef299b58e98b7142c6a3a4292f8e6b47
Yakup <yakupjoraev@gmail.com>
2023-08-19 12:59:41 +0300

    added timer

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,6 +1,43 @@
 // Custom Scripts
 // Custom scripts
 
+// Функция для обновления таймера
+function updateTimer() {
+  // Задаем дату окончания таймера
+  const endDate = new Date("2023-09-25T00:00:00"); // Замените на вашу дату окончания
+  // Получаем текущую дату и время
+  const currentDate = new Date();
+  // Вычисляем разницу между датами в миллисекундах
+  const timeDifference = endDate - currentDate;
+
+  // Если таймер истек, останавливаем интервал и выходим из функции
+  if (timeDifference <= 0) {
+    clearInterval(timerInterval);
+    return;
+  }
+
+  // Вычисляем количество дней, часов, минут и секунд
+  const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+  // Обновляем элементы HTML с отображением времени
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+
+  // Сохраняем данные в localStorage для избежания сброса при обновлении страницы
+  localStorage.setItem("timerData", JSON.stringify({ days, hours, minutes, seconds }));
+}
+
+// Обновляем таймер сразу и устанавливаем интервал для обновления каждую секунду
+updateTimer();
+const timerInterval = setInterval(updateTimer, 1000);
+
+
+
 
 var swiper = new Swiper(".mySwiper", {
   navigation: {
